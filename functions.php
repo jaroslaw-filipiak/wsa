914140855b3e23e212bd72a74dcd3bdb0ac79213
jaroslaw-filipiak <info@j-filipiak.pl>
2021-08-08 12:35:52 +0200

    seo on bottom

--- functions.php
+++ functions.php
@@ -57,10 +57,18 @@ function filter_plugin_updates( $value ) {
     unset( $value->response['advanced-custom-fields-pro/acf.php'] );
     return $value;
 }
-add_filter( 'site_transient_update_plugins', 'filter_plugin_updates' );
-add_filter('acf/settings/show_admin', '__return_false');
+
+// add_filter( 'site_transient_update_plugins', 'filter_plugin_updates' );
+// add_filter('acf/settings/show_admin', '__return_false');
 
 function mytheme_add_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
 	}
 	add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+
+
+
+
+
+remove_action( 'woocommerce_archive_description', 'woocommerce_taxonomy_archive_description', 10 );
+add_action('woocommerce_after_shop_loop', 'woocommerce_taxonomy_archive_description');
